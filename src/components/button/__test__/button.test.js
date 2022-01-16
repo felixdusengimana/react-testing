fff36cb05692b9f9a632299f0c2857a5f230d157
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-16 11:03:49 +0200

    (test): button unit tests

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -1,11 +1,23 @@
-import React from "react"
-import reactDom from "react-dom"
-import Button from "../button"
-import { isTSAnyKeyword } from "@babel/types"
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import renderer from "react-test-renderer"
+import React from "react";
+import reactDom from "react-dom";
+import Button from "../button";
 
+afterEach(cleanup)
 
-it("Renders without crashing.", () => {
+it("renders", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Button label={"Click me please"} />, div)
+  reactDom.render(<Button label={"Click me please"} />, div)
 })
 
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label={"save"} />)
+  expect(getByTestId("button")).toHaveTextContent("save")
+})
+
+it("matches snapshot", () => {
+  const tree = renderer.create(<Button label={"Save"} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
