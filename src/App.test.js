cd1206ba00982f6d498f9306bee80f932e6e8fb7
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-16 12:12:00 +0200

    (test): testing if element is displayed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Click Me Please! button', () => {
   render(<App />);
+  const buttonElement = screen.getByText(/Click Me Please!/i);
+  expect(buttonElement).toBeInTheDocument();
 });
